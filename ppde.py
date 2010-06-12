df55ee9f7923c3ba798ceeec84e68b898edcfa21
Renato Coutinho <renato.coutinho@gmail.com>
2010-06-12 03:02:21 -0300

    Rewriting flux functions using DiffusiveTerm and including 2D diffusion.

--- ppde.py
+++ ppde.py
@@ -12,25 +12,43 @@ from scipy.integrate import odeint
 
 class PDE():
     '''Diffusion equation.'''
-    def __init__(self, **kwargs):
+    def __init__(self, dim=1, **kwargs):
         for (key, value) in kwargs.items():
             setattr(self, key, value)
+        self.dim = dim
 
     def set_grid(self, grid_size):
         self.grid_size = grid_size
         self.dx = self.L / (grid_size+1)
-        self.grid = arange(0, self.L, self.dx)
-        self.grid = self.grid[1:-1]
+        if self.dim == 1:
+            self.grid = arange(0, self.L, self.dx)
+            self.grid = self.grid[1:-1]
 
     def initialize(self, func):
         self.y0 = fromfunction(lambda j: func((j+1)*self.dx), (self.grid_size-1,))
 
+    def DiffusiveTerm_1D(self, u):
+        y = zeros(len(u))
+        y[1:-1] = u[2:] - 2*u[1:-1] + u[:-2]
+        y[0] = self.left - 2*u[0] + u[1]
+        y[-1] = self.right - 2*u[-1] + u[-2]
+        return y/self.dx/self.dx
+   
+    def DiffusiveTerm_2D(self, u):
+        Nx, Ny = self.grid.shape
+        u2 = reshape(u, (Nx, Ny))
+        y = -4*u2
+        y[1:-1,:] += u2[0:-2,:] + u2[2:,:]
+        y[:,1:-1] += u2[:,0:-2] + u2[:,2:]
+        y[0,:] += u2[1,:] + self.top
+        y[-1,:] += u2[-2,:] + self.bottom
+        y[:,0] += u2[:,1] + self.left
+        y[:,-1] += u2[:,-2] + self.right
+        return reshape(y, len(u))
+
     def flux(self, y0, t):
-        y = zeros(len(y0))
-        y[1:-1] = y0[2:] - 2*y0[1:-1] + y0[:-2]
-        y[0] = self.left -2*y0[0] + y0[1]
-        y[-1] = self.right -2*y0[-1] + y0[-2]
-        return self.D/self.dx/self.dx * y
+        y = self.D * self.DiffusiveTerm_1D(y0)
+        return y
 
     def integrate(self, t):
         self.data = odeint(self.flux, self.y0, t)
@@ -39,34 +57,17 @@ class PDE():
 class PDE_fkpp(PDE):
     '''Fisher-Kolmogorov equation.'''
     def flux(self, y0, t):
-        y = zeros(len(y0))
-        y = self.r * y0 * (1. - y0/self.K)
-
-        y[1:-1] += self.D/self.dx/self.dx * (y0[2:] - 2*y0[1:-1] + y0[:-2])
-        y[0] += self.D/self.dx/self.dx * (self.left -2*y0[0] + y0[1])
-        y[-1] += self.D/self.dx/self.dx * (self.right -2*y0[-1] + y0[-2])
+        y = self.r * y0 * (1. - y0/self.K) + self.D * self.DiffusiveTerm_1D(y0)
         return y
 
 
 class PDE_fkpp_competitive(PDE):
     '''Fisher-Kolmogorov equations for 2 competing populations.'''
     def flux(self, y0, t):
-        u = zeros(len(y0)/2)
-        v = zeros(len(y0)/2)
         u0 = y0[:len(y0)/2]
         v0 = y0[len(y0)/2:]
-        
-        u = self.r * u0 * (1. - u0/self.K1 - self.c21 * v0)
-        v = self.r * v0 * (1. - v0/self.K2 - self.c12 * u0)
-        
-        u[1:-1] += self.D1/self.dx/self.dx * (u0[2:] - 2*u0[1:-1] + u0[:-2])
-        u[0] += self.D1/self.dx/self.dx * (self.left -2*u0[0] + u0[1])
-        u[-1] += self.D1/self.dx/self.dx * (self.right -2*u0[-1] + u0[-2])
-
-        v[1:-1] += self.D2/self.dx/self.dx * (v0[2:] - 2*v0[1:-1] + v0[:-2])
-        v[0] += self.D2/self.dx/self.dx * (self.left -2*v0[0] + v0[1])
-        v[-1] += self.D2/self.dx/self.dx * (self.right -2*v0[-1] + v0[-2])
-        
+        u = self.r * u0 * (1. - u0/self.K1 - self.c21 * v0) + self.D1 * self.DiffusiveTerm_1D(u0)
+        v = self.r * v0 * (1. - v0/self.K2 - self.c12 * u0) + self.D2 * self.DiffusiveTerm_1D(v0)
         return concatenate((u, v))
 
     def initialize(self, func):
@@ -75,31 +76,20 @@ class PDE_fkpp_competitive(PDE):
         self.y0 = concatenate((u0, v0))
 
 
-class PDE_poliphenic(PDE):
+class PDE_polyphenic(PDE):
     '''Fisher-Kolmogorov equations for 2 competing populations.'''
     def flux(self, y0, t):
-        N = zeros(len(y0)/3)
-        B = zeros(len(y0)/3)
-        M = zeros(len(y0)/3)
         N0 = y0[:len(y0)/3]
         B0 = y0[len(y0)/3:-len(y0)/3]
         M0 = y0[-len(y0)/3:]
         
-        N = self.r * (B0 + self.g*M0) * (1. - exp(-(B0 + self.g*M0)/self.NC)) - self.s * N0
-        B = -self.mB * B0 + self.s / (1. + N0/self.K) * N0 * (1. - self.b0 * exp(N0-self.NL)/(1.+exp(N0-self.NL)))
-        M = -self.mM * M0 + self.s / (1. + N0/self.K) * N0 * self.b0 * exp(N0-self.NL)/(1.+exp(N0-self.NL))
-        
-        N[1:-1] += self.DN/self.dx/self.dx * (N0[2:] - 2*N0[1:-1] + N0[:-2])
-        N[0] += self.DN/self.dx/self.dx * (self.left -2*N0[0] + N0[1])
-        N[-1] += self.DN/self.dx/self.dx * (self.right -2*N0[-1] + N0[-2])
-
-        B[1:-1] += self.DB/self.dx/self.dx * (B0[2:] - 2*B0[1:-1] + B0[:-2])
-        B[0] += self.DB/self.dx/self.dx * (self.left -2*B0[0] + B0[1])
-        B[-1] += self.DB/self.dx/self.dx * (self.right -2*B0[-1] + B0[-2])
-        
-        M[1:-1] += self.DM/self.dx/self.dx * (M0[2:] - 2*M0[1:-1] + M0[:-2])
-        M[0] += self.DM/self.dx/self.dx * (self.left -2*M0[0] + M0[1])
-        M[-1] += self.DM/self.dx/self.dx * (self.right -2*M0[-1] + M0[-2])
+        N = self.r * (B0 + self.g*M0) * (1. - exp(-(B0 + self.g*M0)/self.NC)) \
+            - self.s * N0 + self.DN * self.DiffusiveTerm_1D(N0)
+        B = -self.mB * B0 + self.s / (1. + N0/self.K) * N0 \
+            * (1. - self.b0 * exp(N0-self.NL)/(1.+exp(N0-self.NL))) \
+            + self.DB * self.DiffusiveTerm_1D(B0)
+        M = -self.mM * M0 + self.s / (1. + N0/self.K) * N0 * self.b0 \
+            * exp(N0-self.NL)/(1.+exp(N0-self.NL)) + self.DM * self.DiffusiveTerm_1D(M0)
         
         return concatenate((N, B, M))
 
@@ -108,7 +98,6 @@ class PDE_poliphenic(PDE):
         B0 = fromfunction(lambda j: func((j+1)*self.dx), (self.grid_size-1,))
         M0 = fromfunction(lambda j: func((j+1)*self.dx), (self.grid_size-1,))
         self.y0 = concatenate((N0, B0, M0))
-
 
 
 def animate(grid, data, skip_frames=1, labelx='x', labely='', labels=[]):
@@ -147,7 +136,6 @@ def PDE_integrate(p, times, equation, grid_size):
     return s
 
 
-
 if __name__ == '__main__':
     p = {
         'r': 2.,        # r
@@ -179,8 +167,8 @@ if __name__ == '__main__':
         'left': 0.,
         'right': 0.
         }
-    times = arange(0, 100, 0.1)
+    times = arange(0, 100, 1)
     #s = PDE_integrate(p, times, equation=PDE_fkpp_competitive, grid_size=200)
-    s = PDE_integrate(p_pp, times, equation=PDE_poliphenic, grid_size=100)
+    s = PDE_integrate(p_pp, times, equation=PDE_polyphenic, grid_size=100)
     animate(s.grid, s.data, labels=['N', 'B', 'M'])
 
